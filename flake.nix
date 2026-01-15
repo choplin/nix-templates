42dfe7dd9d3d6b29dae50873b3798ff5ecfccf18
Akihiro Okuno <choplin.choplin@gmail.com>
2026-01-15 19:50:03 +0900

    feat: add devshell and treefmt configuration
    - Add flake-parts, devshell, and treefmt-nix to root flake
    
    - Configure pre-commit hook to run nix fmt
    
    - Format template files with nixfmt

--- flake.nix
+++ flake.nix
@@ -1,12 +1,70 @@
 {
   description = "Personal Nix flake templates";
 
-  outputs = {self}: {
-    templates = {
-      default = {
-        path = ./templates/default;
-        description = "Development shell with devshell and flake-parts";
-      };
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      devshell,
+      treefmt-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      flake = {
+        templates = {
+          default = {
+            path = ./templates/default;
+            description = "Development shell with devshell and flake-parts";
+          };
+        };
+      };
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+
+          devShells.default = pkgs.devshell.mkShell {
+            imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.taplo.enable = true;
+          };
+        };
+    };
 }
